7e0852c2ff0b97a14f0bb3993b787ba97b693e19
mateojaras <mateo.jaramillo@sofka.com.co>
2022-08-05 16:13:02 -0500

    Version 0.0.1 lista

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -7,20 +7,17 @@ import {
 import { products } from "../data/products";
 import { useShoppingCart } from "../hooks/useShoppingCart";
 import "../styles/custom-styles.css";
-import styles from "../styles/styles.module.css";
+
 
 const product = products[0];
 
 export const ShoppingPage = () => {
-	const { shoppingCart, onProductCountChange } = useShoppingCart();
-
 	return (
 		<div>
 			<h1>Shopping Store</h1>
 
 			<ProductCard
 				product={product}
-				className="bg-dark text-white"
 				key={product.id}
 				initialValues={{
 					count: 4,
@@ -29,20 +26,9 @@ export const ShoppingPage = () => {
 			>
 				{({ reset, increaseBy, isMaxCountReached }) => (
 					<>
-						<ProductImage
-							className="custom-image"
-							style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
-						/>
-						<ProductTittle className="text-bold" />
-						<ProductButtons className="custom-buttons" />
-						<button onClick={reset}>Reset</button>
-						<button onClick={() => increaseBy(-2)}>-2</button>
-						<button
-							className={`${isMaxCountReached && styles.disabled}`}
-							onClick={() => increaseBy(+2)}
-						>
-							+2
-						</button>
+						<ProductImage />
+						<ProductTittle />
+						<ProductButtons />
 					</>
 				)}
 			</ProductCard>
